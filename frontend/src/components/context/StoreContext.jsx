a8d8bc44095758551d76ba8c512ee2c9c161c6e4
Anushrita2003 <anushrita2003@gmail.com>
2026-01-17 00:13:57 +0530

    cart

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -18,7 +18,7 @@ const StoreContextProvider=(props)=>{
     const removeFromCart=(itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
-
+        
     const contextValue={
         food_list,
         cartItems,
