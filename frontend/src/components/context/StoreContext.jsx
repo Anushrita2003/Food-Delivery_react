e64f492f9a3e514965d1c9a7ce025e179794b0ec
Anushrita2003 <anushrita2003@gmail.com>
2026-01-17 21:43:08 +0530

    backend started

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -23,7 +23,7 @@ const StoreContextProvider=(props)=>{
         for(const item in cartItems){
             if(cartItems[item]>0){
                 let itemInfo=food_list.find((product)=>product._id===item);
-                totalAmount+=itemInfo.price*cart[item];
+                totalAmount+=itemInfo.price*cartItems[item];
             }
         }
         return totalAmount;
