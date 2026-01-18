4cf62c6e0fe8bbab3bec96331ac1b2b1341b6b16
Anushrita2003 <anushrita2003@gmail.com>
2026-01-19 00:12:46 +0530

    DB connected_properly

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    try {
-        await mongoose.connect('mongodb+srv://chakrabortyanushrita_db_user:taetUH0dCcpMVSwl@cluster0.aqlkp3n.mongodb.net/FoodDelivery');
-        console.log("DB Connected");
-    } catch (error) {
-        console.log("DB Connection Error:", error);
-    }
+    await mongoose.connect('mongodb+srv://chakrabortyanushrita_db_user:taetUH0dCcpMVSwl@cluster0.aqlkp3n.mongodb.net/FoodDelivery').then(()=>console.log("DB Connected"));
 }
