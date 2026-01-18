1117e3ad336b42fbebc239f8bb4c243e05190d6a
Anushrita2003 <anushrita2003@gmail.com>
2026-01-19 00:05:40 +0530

    dB_connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,10 @@
 import mongoose from "mongoose";
+
+export const connectDB=async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://chakrabortyanushrita_db_user:taetUH0dCcpMVSwl@cluster0.aqlkp3n.mongodb.net/FoodDelivery');
+        console.log("DB Connected");
+    } catch (error) {
+        console.log("DB Connection Error:", error);
+    }
+}
